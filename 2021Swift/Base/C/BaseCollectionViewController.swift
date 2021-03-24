e2c5691caf4f163b68343307bdb36ca8fddbabc5
caolaidong <caolaidong@qq.com>
2021-03-24 17:43:21 +0800

    倒入常用框架

---
+++ 2021Swift/Base/C/BaseCollectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseCollectionViewController.swift
+//  2021Swift
+//
+//  Created by 曹来东 on 2021/3/24.
+//
+
+import UIKit
+
+class BaseCollectionViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 2021Swift/Home/HSJC/V/HSJCCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HSJCCollectionViewCell.swift
+//  2021Swift
+//
+//  Created by 曹来东 on 2021/3/24.
+//
+
+import UIKit
+
+class HSJCCollectionViewCell: BaseCollectionViewCell {
+    
+}
